59a720367616f611f5af66f25e7651b144ccef3f
thajusgamer <lishasajith95@gmail.com>
2024-09-25 21:47:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let username;
+document.getElementById("my-sumit").onclick=function(){
+    username=document.getElementById("my-text").Value;
+    document.getElementById("myh3").textContent=`your now a rishal fan ${username}`
+}
